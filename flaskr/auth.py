6c7160487b5605fdae8737e36d4d266588de9ebd
salchichongallo <salchichongallo1997@gmail.com>
2020-02-21 11:33:40 -0500

    feat(auth): add logout view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -77,3 +77,9 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
